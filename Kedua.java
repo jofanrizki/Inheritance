09f616c44521314740f5e1943d29ba951aee2cd6
jofanrizki <jofan_rizki_29rpl@student.smktelkom-mlg.sch.id>
2021-07-28 20:20:43 +0700

    2021bisa

---
+++ Kedua.java
@@ -0,0 +1,16 @@
+
+package inheritance;
+
+class Kedua extends Pertama
+{
+    private int b = 8;
+    
+    protected void BacaSuper ()
+    {
+        System.out.println ("Nilai b :" +b);
+        terprotek ();
+        info ();
+        
+    }
+    
+}
---
+++ TestPertamaKedua.java
@@ -0,0 +1,17 @@
+
+package inheritance;
+
+class TestPertamaKedua {
+    public static void main(String[] args)
+    {
+        Pertama S1 = new Pertama ();
+        S1.terprotek ();
+        S1.info ();
+        
+        Kedua D2 = new Kedua();
+        D2.BacaSuper ();
+        D2.info();
+        
+
+    }
+}
